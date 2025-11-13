cb11bdcbc62ba84bd8413ac7c871f9e794e2a607
kaliajasih <kaliajasih@gmail.com>
2025-11-13 16:35:07 +0700

    Update profile.js

--- api/profile.js
+++ api/profile.js
@@ -1,4 +1,5 @@
 const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
 
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
@@ -9,14 +10,20 @@ export default async (req, res) => {
     await client.connect();
     const db = client.db('studentapp');
 
-    if (req.method === 'PUT') { // Update profile
+    if (req.method === 'PUT') {
       const { fullname, newUsername, password, kelas } = req.body;
-      await db.collection('users').updateOne({ username }, { $set: { fullname, username: newUsername, password, kelas } });
+      const updateData = { fullname, username: newUsername, kelas };
+      if (password) {
+        updateData.password = await bcrypt.hash(password, 10);
+      }
+      await db.collection('users').updateOne({ username }, { $set: updateData });
       res.status(200).json({ success: true });
-    } else if (req.method === 'DELETE') { // Delete account
+    } else if (req.method === 'DELETE') {
       await db.collection('users').deleteOne({ username });
       await db.collection('notes').deleteMany({ username });
       res.status(200).json({ success: true });
+    } else {
+      res.status(405).json({ error: 'Method not allowed' });
     }
   } catch (error) {
     res.status(500).json({ error: 'Server error' });
